cfea4ed88b354b798430ad89ac026029db22e761
Metaphysiker <s.raess@me.com>
2020-12-29 22:05:47 +0100

    added fullcalendar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :is_user_allowed?, except: [:login]
+
+  def is_user_allowed?
+    unless current_user.nil?
+      unless current_user.role == "admin"
+        #raise "Unauthorized User"
+        sign_out current_user
+        flash[:notice] = "Sie sind nicht authorisiert!"
+        redirect_to new_user_session_path
+      end
+    end
+  end
+
 end
