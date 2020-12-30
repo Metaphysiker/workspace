95646db7e4f40b3bf99b2702f5ec8d1e00c5e152
Metaphysiker <s.raess@me.com>
2020-12-30 09:08:46 +0100

    minus one day

--- app/controllers/event_calendar_controller.rb
+++ app/controllers/event_calendar_controller.rb
@@ -8,7 +8,7 @@ class EventCalendarController < ApplicationController
     end
 
     only_month_days = @date.beginning_of_month..@date.end_of_month
-    calendar_days = @date.beginning_of_month.beginning_of_week..@date.end_of_month.end_of_week + 1.day
+    calendar_days = @date.beginning_of_month.beginning_of_week - 1.day..@date.end_of_month.end_of_week + 1.day
     @events = Event.where(starting: calendar_days).order(:starting)
     #@events = Event.incoming.first(10)
     #byebug
