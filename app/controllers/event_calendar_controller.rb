d24d7766edc123b5261a67d63e866664dca9da06
Metaphysiker <s.raess@me.com>
2021-01-09 18:04:44 +0100

    working on full year

--- app/controllers/event_calendar_controller.rb
+++ app/controllers/event_calendar_controller.rb
@@ -10,7 +10,16 @@ class EventCalendarController < ApplicationController
     only_month_days = @date.beginning_of_month..@date.end_of_month
     calendar_days = @date.beginning_of_month.beginning_of_week - 1.day..@date.end_of_month.end_of_week + 1.day
     @events = Event.where(starting: calendar_days).order(:starting)
-    #@events = Event.incoming.first(10)
-    #byebug
+  end
+
+  def full_year_calendar
+    date = params[:date]
+    if date.nil? || date.empty?
+      @date = Date.today
+    else
+      @date = Date.parse(date)
+    end
+
+    @events = Event.where(starting: @date.beginning_of_year..@date.end_of_year).order(:starting)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :notes
   get '/event_calendar/calendar/(:date)', to: 'event_calendar#calendar', as: 'event_calendar'
+  get '/event_calendar/full_year_calendar/(:date)', to: 'event_calendar#full_year_calendar', as: 'full_year_calendar'
 
   resources :events
   devise_for :users
